7d6ff28a13fe5c9f1a4a15a051ca2d750371646c
sharanagouda.k <sharanagouda.k@robosoftin.com>
2020-07-26 19:27:35 +0530

    button test

--- __tests__/src/components/Button2.test.js
+++ __tests__/src/components/Button2.test.js
@@ -5,6 +5,14 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+
+let mockOpenURL = jest.fn();
+
+// 1. Set openURL module function to jest.fn
+jest.mock('Linking', () => ({
+    openURL: mockOpenURL,
+}));
+
 describe('Button', () => {
     describe('Rendering', () => {
         it('should match to snapshot - Primary', () => {
@@ -14,6 +22,44 @@ describe('Button', () => {
         it('should match to snapshot - Secondary', () => {
             const component = shallow(<Button2 label="test label" primary={false} />)
             expect(component).toMatchSnapshot("Secondary button snapshot")
+        });
+    });
+
+    describe('Interaction', () => {
+        describe('onPressHandler', () => {
+            const mockOnPress = jest.fn();
+            let instance;
+
+            // 2. preparing a reusable shallow rendered instance
+            beforeEach(() => {
+                instance = shallow(<Button2
+                    label="test label"
+                    onPress={mockOnPress}
+                    url="https://www.test.com"
+                />).instance();
+                jest.clearAllMocks();
+            });
+
+            it('should call onPress', () => {
+                instance.onPressHandler();
+                expect(mockOnPress).toHaveBeenCalled();
+                expect(mockOnPress).toHaveBeenCalledTimes(1);
+            });
+
+            it('should call openURL if url is provided', () => {
+                instance.onPressHandler();
+                // 3. mockOpenURL should be called since we have passed in 'url' prop
+                expect(mockOpenURL).toHaveBeenCalled();
+                expect(mockOpenURL).toHaveBeenCalledTimes(1);
+                expect(mockOpenURL).toHaveBeenCalledWith("https://www.test.com");
+            });
+
+            it('should not call openURL if url is nor provided', () => {
+                const innerInstance = shallow(<Button2 label="test label" onPress={mockOnPress} />).instance();
+                innerInstance.onPressHandler();
+                // 4. mockOpenURL should NOT be called as we have NOT passed in 'url' prop
+                expect(mockOpenURL).not.toHaveBeenCalled();
+            });
         });
     });
 });
--- src/scene/Home/index.js
+++ src/scene/Home/index.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import {
     StyleSheet,   // CSS-like styles
     Text,         // Renders text
-    View          // Container component
+    View,          // Container component,
+    Alert
 } from 'react-native';
+import { Button2 } from '../../components';
 
 export default class HomeScreen extends Component {
+    onPress() {
+        Alert.alert("Hello");
+    }
     render() {
         return (
             <View style={[styles.slide, { backgroundColor: '#4AAFEE' }]}>
+                <Button2 label="test label" primary onPress={this.onPress} />
                 <Text style={styles.header}>PRAY</Text>
                 <Text style={styles.text}>Prayer is one of the most important things a Christian can do</Text>
             </View>
